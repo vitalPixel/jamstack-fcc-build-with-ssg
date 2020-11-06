8a756e247bc3a047608c7943d513c190267baba3
Pico <>
2020-11-06 16:50:28 +0000

    descriptive env var

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = async function() {
     try {
-      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=${process.env.API_KEY}`);
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=${process.env.NEWS_API_KEY}`);
       return response.data;
     } catch (error) {
       console.error(error);
